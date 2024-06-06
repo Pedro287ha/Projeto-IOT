183f1c5639d2cddcef9eb210bea6b366207faca1
Pedro_Paulo <143128526+Pedro287ha@users.noreply.github.com>
2024-06-05 23:16:20 -0300

    Tratamento de alguns erros

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import requests
 import json
 from datetime import datetime, timedelta
 import smtplib, ssl
-
+import email
+from time import sleep
 
 ### DEFINICAO DE CONSTANTES DE LOGIN
 ###-----------------------------------------------------------------------------------####
@@ -27,10 +28,14 @@ PORTA = 587  #STARTTLS outlook #
 SERVIDOR_SMTP = "smtp-mail.outlook.com"
 
 ###------------------###
-
-
+# CONFIGURACOES USUARIO
+DIAS_NOTIFICAR = 3
+ID_USUARIO = 116
+HORARIO_LEMBRETE_HORAS = 18
+email_aviso_enviado = False
+###------------------###
 ## Definindo User-agent para evitar block do CLoudFlare
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36'}
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36'}
 
 ###---------------------------------------------------#####
 
@@ -46,12 +51,31 @@ def gerar_api_token(email,senha) -> dict:
 
     # Gerando data para realizar login na API
     data = f"grant_type=password&username={email_encoded}&password={senha_encoded}&language=br&loginType=2" # 
-
-    dados_request = requests.post(url_api_auth,headers=HEADERS, allow_redirects=True,data=data)
     
+    # Tratando possiveis erros de conexao
+    try:
+        dados_request = requests.post(url_api_auth,headers=headers, allow_redirects=True,data=data)
+    
+    except requests.ConnectionError:
+        print("Ocorreu um erro de CONEXAO.. Verfique sua CONEXAO com o server!")
+        return {}
+    except Exception as err:
+        print(f"ERRO desconhecido: {err}")
+        return {}
+        
     # Transformando JSON recebido em um dict 
     dados_dict = dict(json.loads(dados_request.text))
-    
+
+
+    # API respondeu com algum erro ?
+    if dados_dict.get("error") is not None:
+        for erro in dados_dict.values():
+            
+            if erro == "invalid_grant": # Abrindo possibilidade de mais erros da API
+                #print(erro)
+                print("Login ou senha INVALIDOS!!")
+
+                return {}
     
     return dados_dict 
 
@@ -59,22 +83,35 @@ def gerar_api_token(email,senha) -> dict:
 def receber_chamados(api_token,tipo_token) -> list:
     
     lista_chamados = []
-    url_api_auth = "https://liveops-api-americas.seidor.com/v2/orbit/ticket/listwithcount"
+    url_api_chamados = "https://liveops-api-americas.seidor.com/v2/orbit/ticket/listwithcount"
     
     # Alterando Headers da requisicao para enviar request
-    HEADERS["Authorization"] = f"{tipo_token} {api_token}"
-    HEADERS['Content-Type'] = "application/json"
+    headers["Authorization"] = f"{tipo_token} {api_token}"
+    headers['Content-Type'] = "application/json"
     
-    # Json de requisicao usando ID 116 de funcionario
-    data = '''{"TicketExternalIds":null,"InternalTicketNumber":null,"CustomerIds":null,"BusinessIds":null,"ProjectId":null,"AreaId":null,"ProjectTypeIds":null,"TechnicalResourceGroupFilter":[116],"TicketsOpenedByGroupFilter":null,"FilterTicketStatus":null,"MyTicket":false,"FilterDateType":null,"StartDate":null,"EndDate":null,"Term":null,"TicketFilterTermTypes":null,"CustomerUserId":null,"IsOrderByPriority":false,"SquadIds":null,"SquadId":null,"ProjectTypeCodes":null,"ProjectIdsFilter":null,"TicketStatusIdList":null,"DataAnalise":null,"Culture":"br","IsDateTypeAdditionalField":null,"Pager":{"PageSize":100,"PageNumber":1,"SortByField":"updateDate","SortDirection":"desc"}}'''
+    # Json para a API para requisitar chamados
+    data_em_dict = {
+        "TechnicalResourceGroupFilter": [ID_USUARIO],
+        "Pager": {
+            "PageSize": 100,
+            "PageNumber": 1,
+            "SortByField": "updateDate",
+            "SortDirection": "desc"
+            }
+    }
 
-    dados_request = requests.post(url_api_auth,headers=HEADERS, allow_redirects=True,data=data)
+    ## Convertendo data_dicionario para json
+    data = json.dumps(data_em_dict)
+
+    dados_request = requests.post(url_api_chamados,headers=headers, allow_redirects=True,data=data)
     
     # Transformando JSON recebido em um dict 
     dados_dict = dict(json.loads(dados_request.text))
     
-    # Apenas filtrando por status especificos
+    # Apenas filtrando por tipos de status abaixo
     status_importantes = ["Encaminhado para atendente", "Em atendimento"]
+    
+    #d = datetime.today() - timedelta(days=QUANTIDADE_DIAS_CHECAR)
     
     
     for ticket in dados_dict["TicketGrid"]:
@@ -91,36 +128,62 @@ def receber_chamados(api_token,tipo_token) -> list:
             total_dias_entre_datas = int((datetime.now() - data_ticket).days) # Transformando datetime em int
 
             # Chamado esta sem movimento a mais de 3 dias ?
-            if total_dias_entre_datas >= 3:
-
+            if total_dias_entre_datas >= DIAS_NOTIFICAR:
 
                 lista_chamados.append({
                     "id" : ticket["TicketId"],
-                    "Mensagem" : f'ATENCAO o chamado de ID "{ticket["TicketId"]}" esta a {total_dias_entre_datas} dias sem ATUALIZACAO Link: https://liveops-americas.seidor.com/#/ticket/{ticket["TicketId"]} Email: {ticket["TicketAssigneeName"]}',
+                    "corpo_email" : f'<p></p><p><b>ATENÇÃO</b> CHAMADO COM ID <b style="color:purple;">{ticket["TicketId"]}</b> está há <b style="color:red;">{total_dias_entre_datas} dias aberto</b> Link: <a href=https://liveops-americas.seidor.com/#/ticket/{ticket["TicketId"]}>Link Ticket</a></p><img src="https://www.python.org/static/community_logos/python-powered-w-70x28.png"> - Projeto IOT - Email Automático<img><hr>'
                 })
     return lista_chamados
 
-def enviar_email(email_remetente,senha,email_destinatario,mensagem):
 
-    # modelo pradrao da documentacao do SMTPlib
+def enviar_email(email_remetente,senha,email_destinatario,corpo_email):
 
-    mensagem =  f'Subject: CHAMADO ABERTO\n\n{mensagem}\n'
+    # Criando email de envio
+    msg = email.message.Message()
+    msg['Subject'] = "CHAMADO ABERTO" #ASSUNTO DO E-MAIL#
+    msg['From'] = email_remetente #E-MAIL QUE VAI ENVIAR O E-MAIL#
+    msg['To'] = email_destinatario#E-MAIL QUE VAI RECEBER
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(corpo_email)
 
     context = ssl.create_default_context() # criando um handshake com o servidor 
+
     with smtplib.SMTP(SERVIDOR_SMTP, PORTA) as server:
         server.starttls(context=context)
         server.login(email_remetente, senha)
-        server.sendmail(email_remetente, email_destinatario, mensagem)
+        server.sendmail(email_remetente, email_destinatario, msg.as_string().encode('utf-8'))
 
-def main() -> None:
-    token = gerar_api_token(EMAIL_LOGIN_CHAMADOS,SENHA_LOGIN_CHAMADOS)
-    #chamados = get_chamados_abertos(token['access_token'],token['token_type'])
+def main() -> int:
 
-    # Receber todos os chamados abertos e fazer verificacao 
-    chamados = receber_chamados(token['access_token'],token['token_type'])
-    print(len(chamados), chamados)
-    for chamado in chamados:
-        enviar_email(EMAIL_REMETENTE,SENHA_EMAIL_REMETENTE, EMAIL_DESTINATARIO, chamado["Mensagem"].encode('utf8'))
+    ## Usuario informou login e senha de chamados ?
+    
+    if EMAIL_LOGIN_CHAMADOS and SENHA_LOGIN_CHAMADOS:
+
+        token = gerar_api_token(EMAIL_LOGIN_CHAMADOS,SENHA_LOGIN_CHAMADOS)
+        
+        # Executa apenas se token for existente
+        if token:
+            #Receber todos os chamados abertos e fazer verificacao 
+            chamados = receber_chamados(token['access_token'],token['token_type'])
+            print(len(chamados), chamados)
+
+            for chamado in chamados:
+                enviar = enviar_email(EMAIL_REMETENTE,SENHA_EMAIL_REMETENTE, EMAIL_DESTINATARIO, chamado["corpo_email"])
+                    
+
+        
+        ## Loop quando programa estiver rodando
+
+        horario_atual = datetime.now().hour
+        if horario_atual >= HORARIO_LEMBRETE_HORAS and horario_atual <= 23:
+            print("Lembrar de fechar horas!!")
+
+    else:
+        print("Login ou senha NÃO informado!!")
+        return 1
 
 if __name__ == "__main__":
     main()
+
+
